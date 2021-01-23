1a378e5ec2456340ffcf3f8b0fa4b310cb5d6302
ecemtinmaz <ecemtinmaz@gmail.com>
2021-01-23 16:23:27 +0300

    Add project files.

---
+++ week4.HM/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace week4.HM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> ilceSayisi = new Dictionary<string, int>();
+            ilceSayisi.Add("Bursa", 17);
+            ilceSayisi.Add("İstanbul", 39);
+            ilceSayisi.Add("Ankara", 25);
+            ilceSayisi.Add("İzmir", 30);
+
+            foreach (var ilceEslesmesi in ilceSayisi)
+            {
+                Console.WriteLine(ilceEslesmesi);
+            }
+
+        }
+            
+    
+    }
+}
